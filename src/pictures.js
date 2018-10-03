36fcf1cb8ed89e4c04c1f4119ace7a2c1d6cd9cc
Aaron Whitehead <whitehead.aaron@live.com>
2018-10-03 13:19:24 -0700

    almost bad

--- src/pictures.js
+++ src/pictures.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default function Pictures(props) {
-
+   
     return(
 
-        <div className={props.grid} ><img className="boxImg" src={props.imagesrc}/></div>
-    
+         
+         <div className="grid-item" ><img className="boxImg" src={props.imagesrc} /></div>
     );
   
 }
